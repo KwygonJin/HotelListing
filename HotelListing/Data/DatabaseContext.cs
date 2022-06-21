44f4f51742c8f3723610087798721af9eb851eac
Mykola <Kalyan3107@gmail.com>
2022-06-21 15:25:04 +0300

    First commit

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Ukraine",
+                    ShortName = "UA"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                 new Country
+                 {
+                     Id = 3,
+                     Name = "Bahamas",
+                     ShortName = "BS"
+                 }
+             );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Hotel Ukraine",
+                    Address = "Lviv",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Hotel Jamaica",
+                    Address = "Jamaica",
+                    CountryId = 2,
+                    Rating = 4.8
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Hotel Bahamas",
+                    Address = "Bahamas",
+                    CountryId = 3,
+                    Rating = 5
+                }
+             ); 
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ HotelListing/Migrations/20220620140556_DatabaseCreated.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.Migrations
+{
+    public partial class DatabaseCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    Country = table.Column<int>(type: "int", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hotels_Countries_Country",
+                        column: x => x.Country,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hotels_Country",
+                table: "Hotels",
+                column: "Country");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hotels");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
