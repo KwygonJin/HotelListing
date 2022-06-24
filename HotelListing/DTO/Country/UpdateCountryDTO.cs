4bb695ce86af3db58b1fdea3b63d6a98ecb1cc04
Mykola <Kalyan3107@gmail.com>
2022-06-24 20:14:31 +0300

    Forgot DTO

---
+++ HotelListing/DTO/Country/UpdateCountryDTO.cs
@@ -0,0 +1,10 @@
+﻿using HotelListing.DTO.Hotel;
+using System.Collections.Generic;
+
+namespace HotelListing.DTO.Country
+{
+    public class UpdateCountryDTO : CreateCountryDTO
+    {
+        //public virtual IList<HotelDTO> Hotels { get; set; }
+    }
+}
